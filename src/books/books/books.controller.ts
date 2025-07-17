35847c303ef02be80bc1fd6bbe330f652d89260c
Gregor Woiwode <gregor.woiwode@gmail.com>
2025-07-18 01:15:15 +0200

    solution--fundamentals--order-rate-book

--- src/books/books/books.controller.ts
+++ src/books/books/books.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, NotFoundException, Param, Put } from '@nestjs/common';
 import { Book } from '../book/book';
 import { randomUUID } from 'crypto';
+import { DomainException } from '../domain.exception';
 
 @Controller('books')
 export class BooksController {
@@ -19,5 +20,39 @@ export class BooksController {
   @Get()
   getAll(): Promise<Book[]> {
     return Promise.resolve(this.books);
+  }
+
+  @Put(':id/order')
+  order(@Param('id') id: string, @Body() dto: { amountOrdered: number }): void {
+    const bookIndex = this.books.findIndex(book => book.id === id);
+
+    if (bookIndex < 0) {
+      throw new NotFoundException(`Book with id ${id} not found`);
+    }
+
+    const book = this.books[bookIndex];
+
+    if (book.amount < dto.amountOrdered) {
+      throw new DomainException(
+        `Order cancelled: ${dto.amountOrdered} copies of "${book.title}" have been ordered, but only ${book.amount} are available. More will be available, soon!`
+      );
+    }
+
+    book.amount -= dto.amountOrdered;
+
+    this.books[bookIndex] = book;
+  }
+
+  @Put(':id/rate')
+  rate(@Param('id') id: string, @Body() dto: { newRating: number }): void {
+    if (dto.newRating < 1 || dto.newRating > 5) {
+      throw new DomainException(
+        `Rating discarded: The given rating ${dto.newRating} does not fit in the range 1-5.`
+      );
+    }
+
+    this.books = this.books.map(book => {
+      return book.id === id ? { ...book, rating: dto.newRating } : book;
+    });
   }
 }
---
+++ src/books/domain.exception.ts
@@ -0,0 +1,5 @@
+export class DomainException extends Error {
+  constructor(reason: string) {
+    super(reason);
+  }
+}
